3cfe4eeb54aaf57b856f9b98abaccd71e602d276
pdabrow5 <01158021@pw.edu.pl>
2023-02-10 21:32:14 +0100

    Add project files.

---
+++ MazeGameOpenGL.cpp
@@ -0,0 +1,12 @@
+﻿// MazeGameOpenGL.cpp : Defines the entry point for the application.
+//
+
+#include "MazeGameOpenGL.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
